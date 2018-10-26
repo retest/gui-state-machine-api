337fed722cedc5b7534896924efef968f9dbd9c7
Tamino Dauth <tamino@cdauth.eu>
2018-10-26 14:25:17 +0200

    Fix some warnings and remove dead code

--- src/main/scala/de/retest/guistatemachine/JsonFormatForIdMap.scala
+++ src/main/scala/de/retest/guistatemachine/JsonFormatForIdMap.scala
@@ -7,11 +7,15 @@ import spray.json.JsonFormat
 import spray.json.RootJsonFormat
 
 /**
- * Transforms a [[Map]] into a `scala.collection.immutable.Map[String, T]`, so it can be converted into valid JSON.
- * Besides, transforms a JSON object which is a `scala.collection.immutable.Map[String, T]` back into a [[Map]].
+ * Transforms a [[model.Map]] into a `scala.collection.immutable.Map[String, T]`, so it can be converted into valid JSON.
+ * Besides, transforms a JSON object which is a `scala.collection.immutable.Map[String, T]` back into a [[model.Map]].
  * This transformer requires a JSON format for the type `K`.
  */
-class JsonFormatForIdMap[T](implicit val jsonFormat0: JsonFormat[scala.collection.immutable.Map[String, T]], implicit val jsonFormat1: JsonFormat[T]) extends RootJsonFormat[Map[T]] {
+class JsonFormatForIdMap[T](
+  implicit
+  val jsonFormat0: JsonFormat[scala.collection.immutable.Map[String, T]],
+  implicit val jsonFormat1: JsonFormat[T]) extends RootJsonFormat[Map[T]] {
+
   override def write(obj: Map[T]): JsValue =
     jsonFormat0.write(obj.values.map { field => (field._1.id.toString -> field._2) })
 
--- src/main/scala/de/retest/guistatemachine/WebServer.scala
+++ src/main/scala/de/retest/guistatemachine/WebServer.scala
@@ -47,8 +47,5 @@ object WebServer extends App with RestService {
       bindingFuture
         .flatMap(_.unbind()) // trigger unbinding from the port
         .onComplete(_ => system.terminate()) // and shutdown when done
-
-    case None =>
-    // arguments are bad, error message will have been displayed
   }
 }
