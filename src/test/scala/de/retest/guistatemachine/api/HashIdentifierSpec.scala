d9e69ec36746bc0dc4e593f82b9e473ac751bc37
Tamino Dauth <tamino@cdauth.eu>
2019-05-14 15:31:28 +0200

    Increase coverage #34

--- src/test/scala/de/retest/guistatemachine/api/HashIdentifierSpec.scala
+++ src/test/scala/de/retest/guistatemachine/api/HashIdentifierSpec.scala
@@ -17,11 +17,16 @@ class HashIdentifierSpec extends AbstractApiSpec {
     "not equal" in {
       action0Identifier.equals(action1Identifier) shouldEqual false
       action0Identifier.hashCode() should not equal action1Identifier.hashCode()
+      action0Identifier.equals(10) shouldEqual false
     }
 
     "equal" in {
       action0Identifier.equals(action0Identifier) shouldEqual true
       action0Identifier.hashCode() shouldEqual action0Identifier.hashCode()
+    }
+
+    "be converted into a string" in {
+      action0Identifier.toString shouldEqual "HashIdentifier[hash=fd00ea22cb50efd96c3ff59d8900685d0d64f2cee1e77873133e7e186afd2e7f]"
     }
   }
 }
