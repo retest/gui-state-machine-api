3693f9216498966d6853a49e9ece3bea18deff50
Jeremias Roessler <roessler@retest.de>
2020-02-25 22:55:34 +0100

    Improve the info that is written to the state graph gml file

--- src/main/scala/de/retest/guistatemachine/api/SutStateIdentifier.scala
+++ src/main/scala/de/retest/guistatemachine/api/SutStateIdentifier.scala
@@ -3,6 +3,6 @@ package de.retest.guistatemachine.api
 import de.retest.recheck.ui.descriptors.SutState
 
 class SutStateIdentifier(sutState: SutState) extends HashIdentifier(sutState) {
-  val msg = s"SutStateIdentifier[sutState=${sutState.toString}, hash=$hash]"
+  val msg = s"[sutState=${sutState.toString}, hash=${hash.substring(0, 8)}]"
   override def toString: String = msg
 }
